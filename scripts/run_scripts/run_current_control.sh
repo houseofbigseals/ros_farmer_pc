0feb712589500a413f930295f26a4665f7bb7924
greencrow <slonik256@yandex.ru>
2021-03-16 23:28:59 +0300

    added test version of run shell sccripts

---
+++ scripts/run_scripts/run_current_control.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source /opt/ros/melodic/setup.bash;
+
+tun_addr=$(ip -f inet addr show tun0 | awk '/inet / {print $2}' | cut -d/ -f1);
+export ROS_MASTER_URI=http://$tun_addr:11311/;
+export ROS_IP=$tun_addr;
+export ROS_HOSTNAME=$tun_addr;
+
+today=$(date +"%d_%m_%Y_%H:%M:%S");
+outfile_name="${today}_exp_log.out";
+nohup roslaunch ros_farmer_pc current_control.launch > $outfile_name 2>&1 &
