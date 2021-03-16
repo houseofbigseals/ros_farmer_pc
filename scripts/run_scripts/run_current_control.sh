9e60eabf48bf5db926d744fa4e712d5897e48dad
greencrow <slonik256@yandex.ru>
2021-03-16 23:50:49 +0300

    added test version of run shell sccripts

--- scripts/run_scripts/run_current_control.sh
+++ scripts/run_scripts/run_current_control.sh
@@ -6,7 +6,9 @@ tun_addr=$(ip -f inet addr show tun0 | awk '/inet / {print $2}' | cut -d/ -f1);
 export ROS_MASTER_URI=http://$tun_addr:11311/;
 export ROS_IP=$tun_addr;
 export ROS_HOSTNAME=$tun_addr;
+echo "we have set IP as $tun_addr"
 
 today=$(date +"%d_%m_%Y_%H:%M:%S");
-outfile_name="${today}_exp_log.out";
+outfile_name="$HOME/${today}_exp_log.out";
 nohup roslaunch ros_farmer_pc current_control.launch > $outfile_name 2>&1 &
+echo "we have set nohup log path as $outfile_name"
